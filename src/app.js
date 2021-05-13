b93b4bb4ac8416cc2672e923461bae81c69ec40a
stevenwthornton0 <stevenwthornton0@gmail.com>
2021-05-13 18:53:22 -0500

    CORS

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@ const app = express();
   }));
 
   app.use(helmet());
-  app.use(cors());  
+  app.use(cors()); 
+  app.options('*', cors()); 
 
   app.use('/auth', authRouter)
   app.use('/users', usersRouter)
